b5a109bdaab747c77d9556b95913be1be4ca2535
Colin Lu <22761017+SimpleGeometry@users.noreply.github.com>
2017-09-18 19:54:07 -0700

    Create sklearn_linreg.py

---
+++ Linear Regression/sklearn_linreg.py
@@ -0,0 +1,42 @@
+from sklearn import datasets
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+boston = datasets.load_boston()
+X = boston.data[:, 5] #2, 4, 5, 6, 12
+X = X.reshape((X.shape[0], 1))
+y = boston.target
+
+X_train = X[:-100]
+X_test = X[-100:]
+y_train = y[:-100]
+y_test = y[-100:]
+
+h = linear_model.SGDRegressor(penalty='none', eta0=0.03, n_iter=100000)
+h.fit(X_train, y_train)
+
+print('weights:', h.coef_)
+print('bias:', h.intercept_)
+print('r^2:', h.score(X, y))
+
+plt.figure(1)
+plt.subplot(2, 1, 1)
+plt.plot(X_test, y_test, 'bo')
+plt.plot(X_test, h.predict(X_test), 'r-')
+plt.xlabel('Feature')
+plt.ylabel('Price ($10000s of dollars)')
+plt.title('House Price Regression (Test Data)')
+plt.grid(True)
+
+plt.subplot(2, 1, 2)
+plt.plot(X_train, y_train, 'bo')
+plt.plot(X_train, h.predict(X_train), 'r-')
+plt.xlabel('Feature')
+plt.ylabel('Price ($10000s of dollars)')
+plt.title('House Price Regression (Training Data)')
+plt.grid(True)
+
+plt.tight_layout() #prevent text overlapping
+
+plt.show()
