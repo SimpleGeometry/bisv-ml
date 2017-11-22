a7739c7db841dd484cb8e434bc25f132d9719695
Colin Lu <22761017+SimpleGeometry@users.noreply.github.com>
2017-11-21 18:06:14 -0800

    Update mnist.py

--- mnist-tensorflow/mnist.py
+++ mnist-tensorflow/mnist.py
@@ -12,18 +12,19 @@ b1 = tf.Variable(tf.random_normal(shape=(10, 1)))
 
 output = tf.nn.softmax(tf.add(tf.matmul(W1, X), b1))
 
-cost = tf.reduce_mean(tf.reduce_sum(tf.multiply(y, tf.negative(tf.log(output)))))
+#compute the cross entropy (the difference) between the output(prediction) and the actual value
+cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(output), axis=[1]))
 
 optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
   
-  #train the code
+  	#train the model (update weights and biases)
 	for i in range(10000):
 		input_data, output_data = mnist.next_batch()
 		_, loss = sess.run([optimizer, cost], feed_dict={X: input_data, y: output_data})
-    
-    
+   
+	#print accuracy of the model 
 	percent_correct = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(output, 1), tf.argmax(y, 1)), tf.float32))
 	print(sess.run(percent_correct, feed_dict={X: mnist.test.images, y: mnist.test.labels}))
